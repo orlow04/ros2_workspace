5047a2c897c27541f35c4483607e2159dac4d489
orlow04 <gabrielorlow@discente.ufg.br>
2026-05-06 19:43:54 -0300

    melhorias no robo e no .xacro, conexao melhor com o publisher para gazebo e rviz

--- README.md
+++ README.md
@@ -17,10 +17,19 @@ source install/setup.bash
 
 # Visualização no RViz:
 ```bash
-DISPLAY=:1 ros2 launch my_robot display.launch.py
+# Terminal 1 — publish robot description
+DISPLAY=:1 LIBGL_ALWAYS_SOFTWARE=1 ros2 run robot_state_publisher robot_state_publisher \
+  --ros-args -p robot_description:="$(xacro /workspaces/build-your-own-simulated-race-track-cobras-da-robotica/install/real_test_robot/share/real_test_robot/urdf/real_test_robot.urdf.xacro)"
+
+# Terminal 2 — open RViz
+DISPLAY=:1 LIBGL_ALWAYS_SOFTWARE=1 ros2 run rviz2 rviz2
 ```
 
 # Simulação no Gazebo:
 ```bash
-DISPLAY=:1 ros2 launch my_robot gazebo.launch.py
+source install/setup.bash
+DISPLAY=:1 LIBGL_ALWAYS_SOFTWARE=1 ros2 launch real_test_robot rsp.launch.py
 ```
+O modelo de mundo criado para a atividade segue a estrutura de um arquivo SDF, onde são definidos os elementos do ambiente, como luz, chão, linha preta e paredes. Cada elemento é representado por um modelo com suas propriedades específicas, como posição, tamanho e material. O arquivo SDF é utilizado para configurar o ambiente de simulação no Gazebo, permitindo a criação de cenários personalizados para testes e desenvolvimento de robôs.
+
+O chão é uma caixa larga e fina (6 x 6 x 0.02), as paredes também são caixas (6 x 0.1 x 1) posicionadas ao redor do chão para criar um ambiente fechado. Já a linha preta é uma caixa ainda mais fina (0 0 0.021 0 0 0) colocada um pouco acima do chão. A escolha do 0.021 para a linha é justificada pela altura da caixa do chão (0.02) mais uma pequena margem para evitar que a linha fique embutida no chão, o que poderia causar problemas de detecção pelos sensores do robô. Em um primeiro teste testamos apenas uma linha reta, para testar a detecção da linha e o controle do robô, uma vez bem sucedido progredimos para uma linha retangular, buscando testar se o robô consegue "completar a caixa" seguindo a linha.
--- src/real_test_robot/launch/launch_sim.launch.py
+++ src/real_test_robot/launch/launch_sim.launch.py
@@ -1,45 +1,35 @@
 import os
 from launch import LaunchDescription
-from launch.actions import ExecuteProcess, IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch.actions import  DeclareLaunchArgument, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
-from launch.actions import ExecuteProcess
 import xacro
 
 def generate_launch_description():
-    pkg = get_package_share_directory('real_test_robot')
+    
+    package_name = 'real_test_robot'
 
-    # Process xacro into URDF
-    xacro_file = os.path.join(pkg, 'urdf', 'real_test_robot.urdf.xacro')
-    robot_description = xacro.process_file(xacro_file).toxml()
-
-    # Robot state publisher — broadcasts TF from URDF joints
-    robot_state_publisher = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        parameters=[{
-            'robot_description': robot_description,
-            'ignore_timestamp': False
-        }],
-        additional_env={'DISPLAY': ':1', 'LIBGL_ALWAYS_SOFTWARE': '1'}
+    rsp = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','rsp.launch.py'
+                )]), launch_arguments={'use_sim_time': 'true'}.items()
     )
 
-    # Joint state publisher — publishes wheel joint states
-#     joint_state_publisher = Node(
-#         package='joint_state_publisher',
-#         executable='joint_state_publisher',
-#     )
+    world = LaunchConfiguration('world')
+
+    world_arg = DeclareLaunchArgument(
+        'world',
+        default_value='line_follow.sdf',
+        description='SDF world file'
+    )
 
     # Gazebo
     gazebo = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(
-                get_package_share_directory('ros_gz_sim'),
-                'launch', 'gz_sim.launch.py'
-            )
-        ),
-        launch_arguments={'gz_args': '-r empty.sdf'}.items(),
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('ros_gz_sim'),'launch', 'gz_sim.launch.py')]),
+            launch_arguments={'gz_args': ['-r -v4', world], 'on_exit_shutdown': 'true'}.items(),
     )
 
     # Spawn robot into Gazebo
@@ -77,8 +67,8 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
-        robot_state_publisher,
-        # joint_state_publisher,
+        rsp,
+        world_arg,
         gazebo,
         spawn_robot,
         rviz,
